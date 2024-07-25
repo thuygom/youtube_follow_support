a0d35a3c1caf2ec50b0c49e13e17a65954221dfc
thuygom <138266353+thuygom@users.noreply.github.com>
2024-07-25 14:46:06 +0900

    change hiarch

--- module/sql/sqlDb.py
+++ module/sql/sqlDb.py
@@ -16,13 +16,13 @@ if connection.is_connected():
 cursor = connection.cursor()
 
 # 엑셀 파일 읽기
-excel_file_path = '../xlsx/stats0709.xlsx'  # 실제 파일 경로로 변경
+excel_file_path = '../../xlsx/stats0709.xlsx'  # 실제 파일 경로로 변경
 df = pd.read_excel(excel_file_path)
 
-excel_file_path = '../xlsx/crawling_auto0709.xlsx'  # 실제 파일 경로로 변경
+excel_file_path = '../../xlsx/labeling_0709.xlsx'  # 실제 파일 경로로 변경
 df2 = pd.read_excel(excel_file_path)
 
-# 데이터프레임의 각 행을 Youtuber 테이블에 삽입
+#데이터프레임의 각 행을 Youtuber 테이블에 삽입
 for index, row in df.iterrows():
     insert_query = """
     INSERT INTO VideoStat (video_id, upload_date, date, view_count, like_count, comment_count, subscriber_count, channel_id, channel_title, channel_description, topic_categories, title, description, tags, thumbnails)
@@ -49,15 +49,17 @@ for index, row in df.iterrows():
 
 for index, row in df2.iterrows():
         insert_query = """
-        INSERT INTO Comments (comment, author, date, num_likes, video_id)
-        VALUES (%s, %s, %s, %s, %s)
+        INSERT INTO Comments (comment, author, date, num_likes, video_id, emotion, object)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
         """
         data = (
-            row['comment'],
-            row['author'],
-            row['date'],
-            row['num_likes'],
-            row['video_id']
+            row['comment'] if pd.notnull(row['comment']) else None,
+            row['author'] if pd.notnull(row['author']) else None,
+            row['date'] if pd.notnull(row['date']) else None,
+            row['num_likes'] if pd.notnull(row['num_likes']) else None,
+            row['video_id'] if pd.notnull(row['video_id']) else None,
+            row['emotion'] if pd.notnull(row['emotion']) else None,
+            row['object'] if pd.notnull(row['object']) else None,
         )
         cursor.execute(insert_query, data)
         
